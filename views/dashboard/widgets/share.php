8df4851f27cda467a30ed4b3d4ac53667717f6cf
WebCrew <palareas@gmail.com>
2016-12-03 00:28:23 +0100

    First Release
    The first release of TodGmbH theme.

---
+++ views/dashboard/widgets/share.php
@@ -0,0 +1,58 @@
+<div class="slider">
+
+	<!-- STYLE -->
+
+	<link href="<?php echo $this->theme->getBaseUrl() . '/css/orbit.css'; ?>" rel="stylesheet">
+	
+	<!-- BLOCK -->
+
+	<div class="panel panel-default members" id="share-panel">
+		<div class="panel-body">
+		
+			<!-- 
+				IMAGES 
+				Imgaes folder: ../FlatHub/img/orbit/banner
+				Images format: 346x346px jpg
+			-->
+			
+			<div id="featured">
+			    <a href="mailto:andreas_holzer@mail.de" target="_blank">
+				<img src="<?php echo $this->theme->getBaseUrl() . '/img/orbit/banner/1.jpg'; ?>" rel="caption-1" /></a>
+				<a href="http://www.drakensang.com/?aid=1695&invID=xbegegiechbxcifbaggfgxbagaghbigxeibxbgjfxbab&force_instance=481&lang=de" target="_blank">
+				<img src="<?php echo $this->theme->getBaseUrl() . '/img/orbit/banner/2.jpg'; ?>" rel="caption-2" /></a>
+                <a href="http://web-crew.org" target="_blank">
+				<img src="<?php echo $this->theme->getBaseUrl() . '/img/orbit/banner/3.jpg'; ?>" rel="caption-3" /></a>
+				<img src="<?php echo $this->theme->getBaseUrl() . '/img/orbit/banner/4.jpg'; ?>"  rel="caption-4" />
+			</div>
+			
+			<!-- CAPTIONS -->
+
+			<span class="orbit-caption" id="caption-1">Advertise with us! - Click on the banner.</span>
+			<span class="orbit-caption" id="caption-2">Play Drakensang Online! - Click on the banner.</span>
+			<span class="orbit-caption" id="caption-3">The Webdesign Manufactory. - Click on the banner.</span>
+			<span class="orbit-caption" id="caption-4">Invite Your friends to us.</span>
+			
+		</div>
+	</div>
+
+	<!-- JAVASCRIPT -->
+		
+	<script src="<?php echo $this->theme->getBaseUrl() . '/js/jquery.orbit.min.js'; ?>"></script>	
+	<script type="text/javascript">
+		$(window).load(function() {
+			$('#featured').orbit({
+				'animation': 'vertical-slide',		//fade, horizontal-slide, vertical-slide
+				'animationSpeed': 700,				//how fast animtions are
+				'advanceSpeed': 5000,				//if auto advance is enabled, time between transitions 
+				'startClockOnMouseOut': true,		//if clock should start on MouseOut
+				'startClockOnMouseOutAfter': 5000,	//how long after mouseout timer should start again
+				'directionalNav': false,			//manual advancing directional navs
+				'captions': true,					//do you want captions?
+				'captionAnimationSpeed': 700,		//if so how quickly should they animate in
+				'timer': true,						//true or false to have the timer
+				'bullets': true						//true or false to activate the bullet navigation
+			});
+		});
+	</script>
+	
+</div>
---
+++ views/layouts/head.php
@@ -0,0 +1,13 @@
+<!-- MAIN STYLE -->
+
+<link rel="stylesheet" type="text/css" media="screen" title="white-theme" href="<?php echo $this->theme->getBaseUrl() . '/css/theme.css'; ?>">
+<link rel="alternate stylesheet" type="text/css" media="screen" title="dark-theme" href="<?php echo $this->theme->getBaseUrl() . '/css/dark.css'; ?>">
+
+<!-- DARK CSS -->
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/dark.css'; ?>" rel="stylesheet">
+
+
+<!-- OTHER -->
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/lightbox.css'; ?>" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Open+Sans|PT+Sans|Ubuntu+Condensed&subset=latin,cyrillic" rel="stylesheet" type="text/css">
+
