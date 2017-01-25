32de96dba2620dcfc42e10fd2e24cde3559cee48
WebCrew <palareas@gmail.com>
2017-01-25 13:36:12 +0100

    Now markdown in spaces

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -10,3 +10,5 @@
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/lightbox.css'; ?>" rel="stylesheet">
 <link href="//fonts.googleapis.com/css?family=Open+Sans|PT+Sans|Ubuntu+Condensed&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 
+<!-- MARKDOWN IN SPACES -->	
+<?php $this->registerJsFile( $this->theme->getBaseUrl().'/js/humhub_markdown.js', ['position'=>\yii\web\View::POS_END]); ?>
